d853c9a5a0604931c5bf09944486aa10cdf491f0
César Augusto <cesarapolidorio@gmail.com>
2022-05-07 14:14:02 -0300

    test: ensure LessonController return an error if no duration is provided

--- src/presentation/controllers/Lesson/lesson.spec.ts
+++ src/presentation/controllers/Lesson/lesson.spec.ts
@@ -13,4 +13,18 @@ describe("Lesson Controller", () => {
 
         expect(httpResponse.statusCode).toBe(400);
     });
+
+    it("should return an error if no duration is provided", () => {
+        const sut = new LessonController();
+
+        const httpRequest = {
+            body: {
+                description: "any_description",
+            },
+        };
+        const httpResponse = sut.handle(httpRequest);
+
+        expect(httpResponse.statusCode).toBe(400);
+        expect(httpResponse.body).toEqual(new Error("Missing duration on request body"));
+    });
 });
