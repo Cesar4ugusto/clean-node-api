f3ebcfb532c2e38c0abdaaf946f38d774e00e40c
César Augusto <cesarapolidorio@gmail.com>
2022-05-07 14:13:28 -0300

    feat: ensure LessonController return an error if no duration is provided

--- src/presentation/controllers/Lesson/lesson.ts
+++ src/presentation/controllers/Lesson/lesson.ts
@@ -2,6 +2,7 @@ export class LessonController {
     handle(httpRequest: any): any {
         return {
             statusCode: 400,
+            body: new Error("Missing duration on request body"),
         };
     }
 }
