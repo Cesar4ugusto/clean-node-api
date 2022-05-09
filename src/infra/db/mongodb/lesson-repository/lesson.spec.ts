d6cd8d174239dafa5844237889a541637cb38d1c
César Augusto <cesarapolidorio@gmail.com>
2022-05-09 16:17:09 -0300

    refactor: move sut creation to a factory helper

--- src/infra/db/mongodb/lesson-repository/lesson.spec.ts
+++ src/infra/db/mongodb/lesson-repository/lesson.spec.ts
@@ -1,6 +1,10 @@
 import { MongoHelper } from "../helpers/mongo-helper";
 import { LessonMongoRepository } from "./lesson";
 
+const makeSut = (): LessonMongoRepository => {
+    return new LessonMongoRepository();
+};
+
 describe("Lesson Mongo Repository", () => {
     beforeAll(async () => {
         await MongoHelper.connect(process.env.MONGO_URL);
@@ -11,7 +15,7 @@ describe("Lesson Mongo Repository", () => {
     });
 
     it("should return an lesson on success", async () => {
-        const sut = new LessonMongoRepository();
+        const sut = makeSut();
 
         const lesson = await sut.add({ description: "valid_description", duration: 10 });
 
