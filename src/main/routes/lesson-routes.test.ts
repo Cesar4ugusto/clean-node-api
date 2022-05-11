be3fc7e14f2805912769fe0f97af940986aae5c8
César Augusto <cesarapolidorio@gmail.com>
2022-05-11 09:03:40 -0300

    rafactor: connect to mongodb before integration tests

--- src/main/routes/lesson-routes.test.ts
+++ src/main/routes/lesson-routes.test.ts
@@ -1,7 +1,21 @@
 import request from "supertest";
 import app from "../config/app";
+import { MongoHelper } from "@/infra/db/mongodb/mongo-helper";
 
 describe("Lesson Routes", () => {
+    beforeAll(async () => {
+        await MongoHelper.connect(process.env.MONGO_URL);
+    });
+
+    afterAll(async () => {
+        await MongoHelper.disconnect();
+    });
+
+    beforeEach(async () => {
+        const collection = MongoHelper.getCollection("lessons");
+        await collection.deleteMany({});
+    });
+
     it("should return an lesson on success", async () => {
         await request(app).post("/api/lesson").send({ description: "test", duration: 10 }).expect(200);
     });
